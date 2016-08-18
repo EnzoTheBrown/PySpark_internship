29c8a2d7fb65a6af97bf7d5bfab4a84918963a67
EnzoTheBrown <enzo.the@gmail.com>
2016-08-18 11:45:13 -0700

    Update graph_mac.py

--- project/graph_mac.py
+++ project/graph_mac.py
@@ -1,69 +1,98 @@
-import math
+from Heap import myHeap
 
-class myHeap:
+import pickle
+
+pickle_in = open('../data/listener.pickle', 'rb')
+data = pickle.load(pickle_in) 
+
+
+class Node:
+    def __init__(self, mac, visited = False):
+        self.mac = mac
+        self.neighbors = []
+        self.visited = visited
+        self.parent = None
+
+    def add_neighbor(self, node, distance):
+        self.neighbors.append([node, distance])
+
+    def find_neighbor(self, address):
+        for n in self.neighbors:
+            if n[0].mac == address:
+                return n
+        else:
+            print('no such adddress')
+
+    def is_neighbor(self, n):
+        for node in self.neighbors:
+            if node[0] is not None:
+                print(node[0].mac)
+            if n[0] == node:
+                return True
+        return False
+
+    def display_neighbors(self):
+        for n in self.neighbors:
+            print([n[0], n[1]])
+
+
+class Heap(myHeap):
+    @staticmethod
+    def compare(left, right):
+        if (left[1] > right[1]):
+            return True
+        return False
+
+
+class Graph:
     def __init__(self):
-        self.heap = []
-    def push(self,
-             value):
-        self.heap.append(value)
-        current = len(self.heap) - 1
-        if len(self.heap) != 0:
-            while True:
-                if (self.compare(self.heap[int((current - 1) / 2)], self.heap[current])):
-                    self.swap(current, int((current - 1) / 2))
-                current = int((current - 1) / 2)
-                if (current == 0 or (self.compare(self.heap[current], self.heap[int((current - 1) / 2)]))):
-                    break
-        self.first = self.heap[0]
-        self.last = self.heap[len(self.heap) - 1]
-
-    def pop(self):
-        self.swap(0, len(self.heap) - 1)
-        max = self.heap.pop()
-        current = 0
-        while True:
-            child = current*2 + 1
-            if child < len(self.heap) and child + 1 >= len(self.heap):
-                if self.compare(self.heap[child], self.heap[current]):
-                    self.swap(current, child)
-                break
-            if child >= len(self.heap) or child + 1 >= len(self.heap):
-                break
-            if self.compare(self.heap[child], self.heap[child + 1]):
-                child += 1
-            if self.compare(self.heap[current], self.heap[child]):
-                self.swap(current, child)
-            else:
-                break
-            current = child
-        return max
-
-    def swap(self,
-             a,
-             b):
-        temp = self.heap[a]
-        self.heap[a] = self.heap[b]
-        self.heap[b] = temp
-
-    def display(self):
-        print(self.heap)
-
-    def is_heap(self):
+        self.nodes = []
+        for d in data:
+            self.nodes.append(Node(d[0]))
         i = 0
-        while i < len(self.heap):
-            if self.compare(self.heap[int((i - 1) / 2)], self.heap[i]):
-                return False
+        for node in self.nodes:
+            for add in data[i][1]:
+                n = self.find_node(add[1])
+                if n is not None:
+                    node.add_neighbor(self.find_node(n.mac), add[0])
             i += 1
-        return True
 
-    def compare(self,
-                left,
-                right):
-        if(left[1] > right[1]):
-            return True
-        return False
-    def empty(self):
-        if len(self.heap) == 0:
-            return True
+    def find_node(self, mac):
+        for node in self.nodes:
+            if mac in node.mac:
+                return node
+        return None
+
+    def is_correct(self, mac1, mac2):
+        n1 = self.find_node(mac1)
+        n2 = self.find_node(mac2)
+        if n1 is not None and n2 is not None:
+            return n1.is_neighbor(n2)
         return False
 
+    def dijkstra(self, start, goal):#node, node
+        candidates = Heap()
+
+        def push_neighbors(pop):
+            for n in pop[0].neighbors:
+                if not n[0].visited:
+                    n[0].visited = True
+                    candidates.push([n[0], n[1] + pop[1]])
+                    n[0].parent = pop[0]
+
+        candidates.push([start, 0])
+        start.visited = True
+        popped = candidates.pop()
+        while popped[0].mac != goal.mac:
+            push_neighbors(popped)
+            popped = candidates.pop()
+        for n in self.nodes:
+            n.visited = False
+
+        node = goal
+        while node.parent is not None:
+            print([node.mac[:-1], node.parent.find_neighbor(node.mac)[1]])
+            node = node.parent
+
+g = Graph()
+g.dijkstra(g.find_node('e8:50:8b:5d:b1:8d'), g.find_node('2c:54:cf:fa:7f:09'))
