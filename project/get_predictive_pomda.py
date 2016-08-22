8131b6adbefe7a542a2f93f9ba3037c213ffecca
Enzo Lebrun <enzo.the@gmail.com>
2016-08-22 11:31:53 -0700

    link_pomda

--- project/get_predictive_pomda.py
+++ project/get_predictive_pomda.py
@@ -12,13 +12,11 @@ res = sc\
     .textFile('hdfs://master:54310/user/hduser1/wifi')\
     .map(lambda line: re.findall(r'[^\s]+', line))\
     .filter(lambda line: len(line) == 7)\
-    .map(lambda line: ((line[3], line[5], line[0]), 1))\
-    .reduceByKey(add)\
-    .map(lambda line: (line[0][0], (line[1], line[0][1], line[0][2])))\
+    .map(lambda line: (line[3], (line[5], line[0])))\
     .groupByKey().mapValues(list)\
     .collect()
 
-with open('listener.pickle', 'wb') as f:
+with open('listener_pomda.pickle', 'wb') as f:
         pickle.dump(res, f)
 
 
