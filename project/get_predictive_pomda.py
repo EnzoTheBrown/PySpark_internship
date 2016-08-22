7a40a623e6040dad520292c5564c6e58ab88ca3b
Enzo Lebrun <enzo.the@gmail.com>
2016-08-22 12:58:39 -0700

    link_every_days_hours

--- project/get_predictive_pomda.py
+++ project/get_predictive_pomda.py
@@ -6,6 +6,20 @@ from Monk import Monk
 
 monk = Monk()
 
+def count_repeat(actions):
+    old = actions[0][0]
+    it = 0
+    list = actions[1]
+    new_list = []
+    for action in list:
+        if action != old:
+            new_list.append((old, it))
+            it = 1
+            old = action
+        else:
+            it += 1
+    new_list.append((old, it))
+    return [actions[0][0], (actions[0][1], new_list)]
 
 sc = SparkContext('spark://master:7077', 'My KunFu Is The Best')
 res = sc\
@@ -13,9 +27,8 @@ res = sc\
     .map(lambda line: re.findall(r'[^\s]+', line))\
     .filter(lambda line: len(line) == 7)\
     .map(lambda line: ((line[3], line[0]), line[5]))\
-    .distinct()\
     .groupByKey().mapValues(list)\
-    .map(lambda line: (line[0][0], (line[0][1], line[1])))\
+    .map(lambda line: count_repeat(line))\
     .groupByKey().mapValues(list)\
     .collect()
 
